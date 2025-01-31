d77b0f819f0600dc1d8a3fa2d6b187e8da6d6527
goryo4 <yolan.gorget@orange.fr>
2025-01-31 10:45:48 +0100

    philo

---
+++ t.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t.c                                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yolan <yolan@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/19 18:08:26 by yolan             #+#    #+#             */
+/*   Updated: 2025/01/13 16:00:42 by yolan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <sys/time.h>
+#include <stdio.h>
+#include <pthread.h>
+#include "philosopher.h"
+
+/*struct    timeval  
+{
+  time_t        tv_sec ;   //used for seconds
+  suseconds_t       tv_usec ;   //used for microseconds
+};*/
+
+int main() 
+{
+  printf("%d", TIMES)
+  return 0;
+}
